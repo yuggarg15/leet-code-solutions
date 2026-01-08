d8c8aa8cd90a8d1d3541a0f51bab3c58e160ed19
yuggarg15 <yuggarg1505@gmail.com>
2026-01-08 16:47:16 +0530

    Implement maxSubArray function in Solution class

---
+++ q53.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int sum =0; 
+        int maxsum = nums[0] ; 
+        for(int i = 0 ; i < n ; i++ ){     
+           sum += nums[i];
+    
+        if(sum>maxsum ){
+            maxsum = sum ; 
+        }
+        if( sum < 0 ){
+            sum =  0 ;
+        }
+        
+        }
+        return maxsum ;
+    }
+};
