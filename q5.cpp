b3d3f45a5c01c06a4e3a9566a8cdba3043f9613e
yuggarg15 <yuggarg1505@gmail.com>
2026-01-08 16:35:46 +0530

    Create q5.cpp

---
+++ q5.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+    string ans ; 
+    ans = s[0];
+    int left = 0 , right = 0 ;
+    for(int i = 0 ; i < s.length() ; i++){
+        int l = 0 ; 
+        for(int j = 0 ; j < i ; j++){
+            
+        }
+    }
+    }
+};
