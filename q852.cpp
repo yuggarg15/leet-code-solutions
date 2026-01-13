d7bb229c5dfb2d9260d4ce4c164c14ee008e7964
yuggarg15 <yuggarg1505@gmail.com>
2026-01-13 14:29:35 +0530

    Create q852.cpp

---
+++ q852.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int st = 0 , end = arr.size() - 1 ; 
+        while(st<=end) {
+            int mid = st +(end - st)/2 ;
+            if(arr[mid]>=arr[mid-1] && arr[mid]>=arr[mid+1]) {               
+                return mid ;
+            }
+            if(arr[mid]<=arr[mid+1] && arr[mid]>=arr[mid-1]) {
+                st = mid  ;
+            }if(arr[mid]>=arr[mid+1] && arr[mid]<=arr[mid-1]) {
+                end = mid ;
+            }
+
+        }   
+        return 0;    
+    }
+};
