f441cd08607a28abd4b25231237a017e7a071e22
yuggarg15 <yuggarg1505@gmail.com>
2026-01-15 16:39:06 +0530

    Create q2894.cpp

---
+++ q2894.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int sum = 0 ;
+        for(int i = 1; i <=n ; i++){
+            if(i%m){
+                sum+=i;
+            }else{
+                sum-=i;
+            }
+        }
+        
+        return sum ; 
+    }
+};
