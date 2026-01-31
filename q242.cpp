e33cc65c7272a17e64fe4d19e05f288c4a9d0ebb
yuggarg15 <yuggarg1505@gmail.com>
2026-01-31 20:50:31 +0530

    Create q242.cpp

---
+++ q242.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> alphabet(26,0);
+        for(int i = 0 ; i<s.size();i++){
+            alphabet[s[i] -'a']++;
+        }
+        for(int i = 0 ; i< t.size(); i++){
+            alphabet[t[i]-'a']--;
+        }
+        for(int i = 0 ; i < 26 ; i ++){
+            if(alphabet[i]){
+                return false ;
+            }
+        }
+        return true;
+    }
+};
